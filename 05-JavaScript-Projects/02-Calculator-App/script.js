25286c646c203dd58442ed31dd5d32d6f04f07cd
Francisco Bajo <fbajo001@gmail.com>
2026-09-04 00:02:45 +0800

    Create JavaScript calculator app

---
+++ 05-JavaScript-Projects/02-Calculator-App/script.js
@@ -0,0 +1,26 @@
+function addValue(value){
+
+document.getElementById("display").value += value;
+
+}
+
+
+function clearDisplay(){
+
+document.getElementById("display").value = "";
+
+}
+
+
+function calculate(){
+
+let result =
+eval(document.getElementById("display").value);
+
+document.getElementById("display").value = result;
+
+}
+
+function backspace() {
+    document.getElementById("display").value = document.getElementById("display").value.slice(0, -1);
+  }
